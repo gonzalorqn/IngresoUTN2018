c58dd050ba177d67f5bc93ad2c89fb96fe9027ee
gonzalorqn <36246342+gonzalorqn@users.noreply.github.com>
2018-02-15 09:33:34 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,25 @@
 function Mostrar()
 {
 //tomo el mes 
-var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
+var mes = document.getElementById("mes").value;
 
-
+switch (mes)
+{
+    case "Febrero":
+        alert("Este mes tiene 28 dias.");
+        break;
+    
+    case "Abril":
+    case "Junio":
+    case "Septiembre":
+    case "Noviembre":
+        alert("Este mes tiene 30 dias.");
+        break;
+   
+    default:
+        alert("Este mes tiene 31 dias.");
+        break;
+}
 
 }//FIN DE LA FUNCIÓN
