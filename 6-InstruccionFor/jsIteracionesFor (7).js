83244bebeaed6ad1bf49979fb60234851bb0f813
gonzalorqn <36246342+gonzalorqn@users.noreply.github.com>
2018-02-22 12:26:17 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,6 +1,21 @@
 function Mostrar()
 {
+var cantDivisores=0;
+var pedir = parseInt(prompt("Ingrese un número."))
+for(i=0 ; pedir >= i ; i++)
+{
+    if(pedir % 2 == 0)
+    {
+        console.log(pedir);
+        cantDivisores++;
+    }
+    else
+    {
+        pedir++;
+    }
+}
 
+alert("La cantidad de pares es: " + cantDivisores);
 
 
 
